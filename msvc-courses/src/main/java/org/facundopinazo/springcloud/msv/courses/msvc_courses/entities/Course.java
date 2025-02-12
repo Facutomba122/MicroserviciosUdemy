332fa20b43505837dcf0de62436906a4b556c30d
Facundo Pinazo <facundoelst@gmail.com>
2025-02-11 19:16:00 -0500

    add user DTO in Course

--- msvc-courses/src/main/java/org/facundopinazo/springcloud/msv/courses/msvc_courses/entities/Course.java
+++ msvc-courses/src/main/java/org/facundopinazo/springcloud/msv/courses/msvc_courses/entities/Course.java
@@ -1,7 +1,9 @@
 package org.facundopinazo.springcloud.msv.courses.msvc_courses.entities;
 
 import jakarta.persistence.*;
+import org.facundopinazo.springcloud.msv.courses.msvc_courses.entities.DTOs.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -13,7 +15,20 @@ public class Course {
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL)
-    private List<CourseUser> courseUsers;
+    @JoinColumn(name = "course_id")
+    private List<CourseUser> courseUsers = new ArrayList<CourseUser>();
+
+    @Transient
+    private List<User> users = new ArrayList<User>();;
+
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 
     public List<CourseUser> getCourseUsers() {
         return courseUsers;
